a03c71610b2d9f7c58e57a16a58f65f922c2fd10
jng308 <99092173+jng308@users.noreply.github.com>
2022-02-26 22:11:41 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -92,10 +92,11 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
        # print('250 reply not received from server.')
     # Fill in end
     
-    clientSocket.close()
+    
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
 SMTP_Server_skeleton.py
 Displaying SMTP_Server_skeleton.py.
 
+clientSocket.close()

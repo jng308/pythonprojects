559d56847119cbcabcbbb86448edc15644045484
jng308 <99092173+jng308@users.noreply.github.com>
2022-04-15 16:57:51 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -48,16 +48,14 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
-        header = recPacket[20: 28]
-        type, code, checksum, packetID, sequence = struct.unpack("!bbHHh", header)
-        if type == 0 and packetID == ID:  # type should be 0
-            byte_in_double = struct.calcsize("!d")
-        timeSent = struct.unpack("!d", recPacket[28: 28 + byte_in_double])[0]
-        delay = (timeReceived - timeSent) * 1000
-        ttl = ord(struct.unpack("!c", recPacket[8:9])[0].decode())
-        return (delay, ttl, byte_in_double)
-        # Fetch the ICMP header from the IP packet
-
+        icmpHeader = recPacket[20:28]
+ icmpType, code, myChecksum, packetID, sequence = struct.unpack("bbHHh",
+icmpHeader)
+ if icmpType != 8 and packetID == ID:
+ bytesInDouble = struct.calcsize("d")
+timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+ rtt = (timeReceived - timeSent) * 1000
+ return rtt
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:

07926afd49f9538e8887b4e787b8a3a092ec0e90
jng308 <99092173+jng308@users.noreply.github.com>
2022-04-16 00:13:12 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import struct
 import time
 import select
 import binascii
-
 # Should use stdev
 
 ICMP_ECHO_REQUEST = 8
@@ -34,6 +33,7 @@ def checksum(string):
     return answer
 
 
+
 def receiveOnePing(mySocket, ID, timeout, destAddr):
     timeLeft = timeout
 
@@ -48,15 +48,15 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
-        header = recPacket[20: 28]
-        type, code, checksum, packetID, sequence = struct.unpack("!bbHHh", header)
-        if type == 0 and packetID == ID: # type should be 0
-            byte_in_double = struct.calcsize("!d")
-            timeSent = struct.unpack("!d", recPacket[28: 28 + byte_in_double])[0]
-            delay = (timeReceived - timeSent) * 1000
-            ttl = ord(struct.unpack("!c", recPacket[8:9])[0].decode())
-            return (delay, ttl, byte_in_double)
+
         # Fetch the ICMP header from the IP packet
+        icmpHeader = recPacket[20:28]
+        icmpType, code, myChecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+        if icmpType != 8 and packetID == ID:
+            bytesInDouble = struct.calcsize("d")
+        timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+        rtt = (timeReceived - timeSent) * 1000
+        return rtt
 
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
@@ -83,19 +83,21 @@ def sendOnePing(mySocket, destAddr, ID):
     else:
         myChecksum = htons(myChecksum)
 
+
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
 
     mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
 
+
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
-
 
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
 
-    # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
+
+    # SOCK_RAW is a powerful socket type. For more details:   https://sock-raw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
 
     myID = os.getpid() & 0xFFFF  # Return the current process i
@@ -106,21 +108,29 @@ def doOnePing(destAddr, timeout):
 
 
 def ping(host, timeout=1):
-    # timeout=1 means: If one second goes by without a reply from the server,      # the client assumes that either the client's ping or the server's pong is lost
+    # timeout=1 means: If one second goes by without a reply from the server,  	
+    # the client assumes that either the client's ping or the server's pong is lost
     dest = gethostbyname(host)
-    #print("Pinging " + dest + " using Python:")
-    #print("")
-
-    # Send ping requests to a server separated by approximately one second
-    # Add something here to collect the delays of each ping in a list
-    # so you can calculate vars after your ping
-    for i in range(0, 4):
+    print("Pinging " + dest + " using Python:")
+    print("")
+    
+    #Send ping requests to a server separated by approximately one second
+    #Add something here to collect the delays of each ping in a list so you can calculate vars after your ping
+    
+    for i in range(0,4): #Four pings will be sent (loop runs for i=0, 1, 2, 3)
         delay = doOnePing(dest, timeout)
-        #print(delay)
+        print(delay)
         time.sleep(1)  # one second
+    packet_min = min(delay_float)
+    packet_max = max(delay_float)
+    packet_avg = (sum(delay_float))/(len(delay_float))
+    stdev_var = stdev(delay_float)
+    #vars = packet_min, packet_avg, packet_max, stdev_var
+        
+    #You should have the values of delay for each ping here; fill in calculation for packet_min, packet_avg, packet_max, and stdev
+    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
 
     return vars
-
 
 if __name__ == '__main__':
     ping("google.co.il")
